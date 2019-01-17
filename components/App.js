d8c347dc6a59021318ff7ce4fd1f9160d3a2612a
Pawel Oczkowski <pawel.oczko84@gmail.com>
2019-01-17 11:49:58 +0100

    Add Promise

---
+++ components/App.js
@@ -0,0 +1,77 @@
+var GIPHY_PUB_KEY = 'XtUgXFCOWmOujJ4McHMnnfmp77PdRhwX';
+var GIPHY_API_URL = 'https://api.giphy.com'
+
+App = React.createClass({
+            getInitialState() {
+                return {
+                    loading: false,
+                    searchingText: '',
+                    gif: {}
+                };
+            },
+            handleSearch: function (searchingText) {
+                this.setState({
+                    loading: true
+                });
+                this.getGif(searchingText)
+                .then(gif => {
+                    this.setState({
+                        loading: false,
+                        gif: gif,
+                        searchingText: searchingText
+                    })
+                })
+                    .catch(error => console.error('Something went wrong', error));
+            },
+
+    getGif: function (searchingText) {
+        return new Promise((resolve, reject) => {
+            var url =
+                GIPHY_API_URL +
+                "/v1/gifs/random?api_key=" +
+                GIPHY_PUB_KEY +
+                "&tag=" +
+                searchingText;
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", url);
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    var data = JSON.parse(xhr.responseText).data;
+                    if (data.type === "gif") {
+                        var gif = {
+                            url: data.fixed_width_downsampled_url,
+                            sourceUrl: data.url
+                        };
+                        resolve(gif);
+                    } else {
+                        reject(new Error(this.statusText));
+                    }
+                }
+            };
+            xhr.onerror = () => {
+                reject(new Error(
+                    `XMLHttpRequest Error: ${this.statusText}`
+                ));
+            }
+            xhr.send();
+        });
+    },
+
+                render: function () {
+                    var styles = {
+                    margin: '0 auto',
+                    textAlign: 'center',
+                    width: '90%'
+                    };
+
+                    return (
+                    <div style={styles}>
+                        <h1> Wyszukiwarka GIFow!</h1>
+                        <p> Znajdź gifa na<a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.
+                        </p>
+                        <Search onSearch={this.handleSearch} />
+                        <Gif loading={this.state.loading} url={this.state.gif.url} sourceUrl={this.state.gif.sourceUrl} />
+                    </div>
+                    );
+               }
+});
